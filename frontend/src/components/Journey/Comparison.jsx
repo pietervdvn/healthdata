26c52ad4e1f59182fdcc7e1a4a1f220bcd28cd3f
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2018-07-17 14:44:33 +0200

    show 100 dummies

--- frontend/src/components/Journey/Comparison.jsx
+++ frontend/src/components/Journey/Comparison.jsx
@@ -6,18 +6,42 @@ import Footer from '../Footer';
 
 import { Link } from 'react-router-dom';
 
-const Comparison = () => (
+class Comparison extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            listOfDummies: new Array(),
+        };
+        this.fillList();
+
+    }
+    fillList() {
+        for (let i = 0; i < 100; i++) {
+            this.state.listOfDummies.push(<Dummy />);
+        }
+    }
+    render() {
+        return (
+            <div>
+                <Navbar />
+                <div className="journey_content">
+                    Comparison
+                    <br />
+                        {this.state.listOfDummies}
+                    <br />
+                    <Link to="/journey/map">Continue</Link>
+                </div>
+                <Footer />
+            </div>
+        );
+    }
+}
+
+const Dummy = () => (
     <div>
-        <Navbar />
-        <div className="journey_content">
-            Comparison
-            <br />
-            <Link to="/journey/map">Continue</Link>
-        </div>
-        <Footer />
+        Oi
     </div>
-
 );
 
 
