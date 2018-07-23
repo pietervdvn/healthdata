68a7d1df7c5cb4cb39840eef71574b5d7c010e62
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2018-07-23 09:43:18 +0200

    update App routingm remove the redundant ones

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,6 @@ import Home from './components/Home';
 import Explorer from './components/Explorer';
 import Journey from './components/Journey';
 import NotFound from './components/404';
-import Questions from './components/Journey/Questions';
-import Persona from './components/Journey/Persona';
-import Map from './components/Journey/Map';
-import ComparisonProvince from './components/Journey/ComparisonProvince';
-import ComparisonBelgium from './components/Journey/ComparisonBelgium';
 
 
 
@@ -20,12 +15,6 @@ class App extends React.Component {
           <Route exact path="/:path(|index|home)" component={Home} />
           <Route exact path="/:path(explorer)" component={Explorer} />
           <Route exact path="/:path(journey)" component={Journey} />
-          <Route exact path="/:path(journey)/:path(questions)" component={Questions} />
-          <Route exact path="/:path(journey)/:path(persona)" component={Persona} />
-          <Route exact path="/:path(journey)/:path(comparisonProvince)" component={ComparisonBelgium} />
-          <Route exact path="/:path(journey)/:path(comparisonBelgium)" component={ComparisonProvince} />
-          <Route exact path="/:path(journey)/:path(map)" component={Map} />
-
           <Route component={NotFound} />
         </Switch>
       </BrowserRouter>
