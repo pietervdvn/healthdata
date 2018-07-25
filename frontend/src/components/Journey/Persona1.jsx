46914c59b183abc33cd8a175e400d12e1c0bbff7
QingzhiHu <1552794394@qq.com>
2018-07-25 11:01:33 +0200

    third page css

--- frontend/src/components/Journey/Persona1.jsx
+++ frontend/src/components/Journey/Persona1.jsx
@@ -8,19 +8,22 @@ class Persona1 extends React.Component {
             <div>
                 <div className="flex-container">
                     <div className="personaLeft">
-
+                        <div className="Jonathan"></div>
                     </div>
                     <div className="personaRight">
                         <div className="personaContent">
-                            <h1><span className="redUnderline">Me</span>et {this.props.name}</h1>
+                            <span className="central"><h1><span className="redUnderline">Me</span>et {this.props.name} ......</h1></span>
+                           <div className="littlefont">Diagnosis : Major depressive disorder</div>
+                            <div className="middlefont">
                             <p>
                                 John is a <span className="bold red">{this.props.age}</span> year old man living in
                                 {this.props.gender == "male" ? "man" : "woman"} living in <span className="red bold">{this.props.province}</span>.
                                 {this.props.gender == "male" ? " He" : " She"} enjoys little things in life, he loves to cook, go to the cinema and have friends home for dinner.
                             </p>
                             <p>
-                                However things changed a few years ago...
+                                A few years ago, Jonathan<span className="bold"> stopped feeling happiness and joy.</span> He lost his appetite and started having negative feelings. He got diagnosed with <span className="bold">dysthymia.</span> During the fall it got even worse and eventually he fell into a <span className="bold">major depression</span>. Which is what he deals with everyday now...
                             </p>
+                            </div>
 
                             <p>
                                 <p>
